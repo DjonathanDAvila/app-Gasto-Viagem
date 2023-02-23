9f86fc149a6afc8fe348756f8dd8a245f2370930
Djonathan <djonathandavila.dev@gmail.com>
2023-02-23 10:54:28 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Project: Gasto de Viagem app
+============================
+
+Objetivo
+--------
+
+Colocar em prática alguns conceitos aprendidos:
+
+. Como identificar os elementos de interfae
+. Como capturar os dados
+. Fazer os calculos e validação de dados
+. ViewBinding para reconhecer os elementos de interface 
+. Responder aos cliques do botão
+. Toast notifications para mostrar mensagem ao usuário
---
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.gastoviagem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    /**
+     * Crio a função onClick e os if para se caso app possua mais de um button, cada um seja tratado
+     * de forma diferente conforme forem sendo chamados
+     */
+    override fun onClick(view: View) {
+       if (view.id == R.id.button_calculate) {
+           calculate()
+       }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this , R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
